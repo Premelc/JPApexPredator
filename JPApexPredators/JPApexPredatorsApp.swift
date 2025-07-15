b6c2ad495b05126805fa646e076b2cf27100f88f
Dominik Premelc <dominik.premelc33@gmail.com>
2025-07-15 13:44:16 +0200

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Dominik Premelč on 15.07.2025..
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
