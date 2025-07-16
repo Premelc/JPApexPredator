130cf37677c93911beab8db9de72bee871142597
Dominik Premelc <dominik.premelc33@gmail.com>
2025-07-16 10:13:43 +0200

    Complete JPApex predators course

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -6,19 +6,78 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    let predators = Predators()
+    @State var searchText = ""
+    @State var alphabetical = false
+    @State var currentSelection = APType.all
+    
+    var filteredDinos: [ApexPredator]{
+        predators.filter(by: currentSelection)
+        predators.sort(by: alphabetical)
+        return predators.search(for: searchText)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+        List(filteredDinos) { predator in
+            NavigationLink{
+                PredatorDetail(predator: predator,
+                               position: .camera(MapCamera(centerCoordinate: predator.location, distance: 30000)))
+            } label:{
+                HStack {
+                    Image(predator.image).resizable().scaledToFit().frame(width: 100, height: 100).shadow(color: .white,radius: 1)
+                    VStack(alignment: .leading){
+                        Text(predator.name)
+                            .fontWeight(.bold)
+                        
+                        Text(predator.type.rawValue.capitalized)
+                            .fontWeight(.semibold)
+                            .padding(.horizontal , 13)
+                            .padding(.vertical , 5)
+                            .background(predator.type.background)
+                            .clipShape(.capsule)
+                    }
+                }
+            }
+            }
+        .navigationTitle("Apex predators")
+        .searchable(text: $searchText)
+        .autocorrectionDisabled()
+        .animation(.default , value: searchText)
+        .toolbar{
+            ToolbarItem(placement: .topBarLeading){
+                Button{
+                    withAnimation{
+                        alphabetical.toggle()
+                    }
+                } label: {
+                    Image(systemName: alphabetical ? "textformat" : "film").symbolEffect(.bounce, value: alphabetical)
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing){
+                Menu{
+                    Picker(
+                        "Filter",
+                        selection: $currentSelection.animation()
+                    ) {
+                        ForEach(APType.allCases) { type in
+                            Label(type.rawValue.capitalized, systemImage: type.icon)
+                        }
+                    }
+                } label: {
+                    Image(systemName: "slider.horizontal.3")
+                }
+            }
         }
-        .padding()
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
 #Preview {
     ContentView()
 }
+ 
---
+++ JPApexPredators/PredatorMap.swift
@@ -0,0 +1,55 @@
+//
+//  PredatorMap.swift
+//  JPApexPredators
+//
+//  Created by Dominik Premelč on 16.07.2025..
+//
+
+import SwiftUI
+import MapKit
+
+struct PredatorMap: View {
+    let predators = Predators()
+    @State var position: MapCameraPosition
+    @State var satellite = false
+    var body: some View {
+        Map(
+            position: $position
+        ){
+            ForEach(predators.apexPredators){ predator in
+                Annotation(predator.name , coordinate: predator.location){
+                    Image(predator.image).resizable().scaledToFit().frame(height: 100).shadow(color: .white, radius: 3).scaleEffect(x: -1)
+                }
+            }
+        }.mapStyle( satellite ?  .imagery(elevation: .realistic) : .standard(elevation: .realistic))
+            .overlay(alignment: .bottomTrailing){
+                Button{
+                    satellite.toggle()
+                } label: {
+                    Image(systemName: satellite ? "globe.americas.fill" : "globe.americas")
+                        .font(.largeTitle)
+                        .imageScale(.large)
+                        .padding(3)
+                        .background(.ultraThinMaterial)
+                        .clipShape(.rect(cornerRadius: 7))
+                        .shadow(radius: 3)
+                        .padding()
+                }
+            }
+            .toolbarBackground(.automatic)
+    }
+}
+
+#Preview {
+    var predator = Predators().apexPredators[2]
+    PredatorMap(
+        position: .camera(
+            MapCamera(
+                centerCoordinate: predator.location,
+                distance: 1000,
+                heading: 250,
+                pitch: 80
+                )
+            )
+    ).preferredColorScheme(.dark)
+}
